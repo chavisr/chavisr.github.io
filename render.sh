1f5ee93587f1678087ae0a70f63da7977833b725
Chavis Rujichan <chavis.rujichan@gmail.com>
2025-09-05 16:19:22 +0700

    update

--- render.sh
+++ render.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -6,7 +6,7 @@ VENV_DIR="venv"
 
 [ ! -d "$VENV_DIR" ] && python3 -m venv "$VENV_DIR"
 
-source "$VENV_DIR/bin/activate"
+. "$VENV_DIR/bin/activate"
 
 pip install -r requirements.txt
 
